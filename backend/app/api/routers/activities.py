fbb5b2d3c6ace6a271e427a2c1387bb0c3a8f242
jorgeeemera <jorge.mera@udc.es>
2026-05-18 18:58:56 +0200

    feat: add activities endpoint

--- backend/app/api/routers/activities.py
+++ backend/app/api/routers/activities.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter, Depends
-from app.core.security import authenticate
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+
+# Importamos el modelo de actividades que crearon tus compañeros
+from app.models.activity import Activity 
 
 router = APIRouter()
 
+# Quitamos el candado (authenticate) y abrimos la conexión a la base de datos (get_db)
 @router.get("/activities")
-def get_activities(user = Depends(authenticate)):
-    return [{"id": 1, "title": "Football Tournament"}]
+def get_activities(db: Session = Depends(get_db)):
+    # Buscamos todas las actividades en la tabla real de la base de datos
+    activities = db.query(Activity).all()
+    return activities
